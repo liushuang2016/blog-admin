ff7061be7bb372c82d43da76c8e561807d5c3343
liushuang <liushuangbill@outlook.com>
2019-01-08 21:03:05 +0800

    fix

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -116,14 +116,14 @@ class BasicLayout extends React.PureComponent {
     const currRouterData = this.matchParamsPath(pathname, breadcrumbNameMap);
 
     if (!currRouterData) {
-      return 'Ant Design Pro';
+      return 'LiuShuang\'s Blog';
     }
     const pageName = formatMessage({
       id: currRouterData.locale || currRouterData.name,
       defaultMessage: currRouterData.name,
     });
 
-    return `${pageName} - Ant Design Pro`;
+    return `${pageName} - LiuShuang's Blog`;
   };
 
   getLayoutStyle = () => {
